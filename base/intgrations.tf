c1d9a1bffee7440a78f90a67ef4f50943508a236
Simen Svenkerud <simen@ullrscrafts.com>
2026-05-19 09:35:29 +0200

    adding security integration

--- base/intgrations.tf
+++ base/intgrations.tf
@@ -47,14 +47,14 @@ resource "snowflake_saml2_integration" "OKTA_SSO" {
 }
 
 
-#resource "snowflake_storage_integration" "s3_integration" {
-#  provider                  = snowflake.accountadmin
-#  count                     = var.snowflake_aws_s3_integration ? 1 : 0
-#  name                      = "S3_STORAGE"
-#  comment                   = "This integration is purposfully left open, and the access limitation is done on the AWS IAM role."
-#  type                      = "EXTERNAL_STAGE"
-#  storage_allowed_locations = ["*"]
-#  enabled                   = true
-#  storage_provider          = "S3"
-#  storage_aws_role_arn      = var.snowflake_aws_s3_integration_role
-#}
+resource "snowflake_storage_integration" "s3_integration" {
+  provider                  = snowflake.accountadmin
+  count                     = var.snowflake_aws_s3_integration ? 1 : 0
+  name                      = "S3_STORAGE"
+  comment                   = "This integration is purposfully left open, and the access limitation is done on the AWS IAM role."
+  type                      = "EXTERNAL_STAGE"
+  storage_allowed_locations = ["*"]
+  enabled                   = true
+  storage_provider          = "S3"
+  storage_aws_role_arn      = var.snowflake_aws_s3_integration_role
+}
